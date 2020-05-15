c589ecd25b8f8065400288610f36ccd50d6ce3b4
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-15 18:19:59 -0400

    converted Promise.all() function to an async await function

--- exerciseAsyncAwait.js
+++ exerciseAsyncAwait.js
@@ -23,9 +23,10 @@ const urls = [
 ]
 
 const getData = async function() {
-  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
-      fetch(url).then(resp => resp.json())
-  ));
+  const [ users, posts, albums ] = await Promise.all(urls.map( async function(url) {
+      const resp = await fetch(url);
+      return resp.json();
+  }));
   console.log('users', users);
   console.log('posta', posts);
   console.log('albums', albums);
